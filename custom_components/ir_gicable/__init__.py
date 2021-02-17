a77dc21018281819689baaf55c529c7f9a6f6827
Alex <alexyao2015@gmail.com>
2021-02-17 15:15:30 -0500

    Fix loading

--- custom_components/ir_gicable/__init__.py
+++ custom_components/ir_gicable/__init__.py
@@ -1,21 +1,11 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome import automation
-from esphome.components import binary_sensor
-from esphome.const import CONF_DATA, CONF_TRIGGER_ID, CONF_NBITS, CONF_ADDRESS, \
-    CONF_COMMAND, CONF_CODE, CONF_PULSE_LENGTH, CONF_SYNC, CONF_ZERO, CONF_ONE, CONF_INVERTED, \
-    CONF_PROTOCOL, CONF_GROUP, CONF_DEVICE, CONF_STATE, CONF_CHANNEL, CONF_FAMILY, CONF_REPEAT, \
-    CONF_WAIT_TIME, CONF_TIMES, CONF_TYPE_ID, CONF_CARRIER_FREQUENCY, CONF_RC_CODE_1, CONF_RC_CODE_2
-from esphome.core import coroutine
-from esphome.util import Registry, SimpleRegistry
-
-from esphome.components.remote_base import declare_protocol, register_binary_sensor, register_trigger \
+from esphome.const import CONF_ADDRESS, CONF_COMMAND
+from esphome.components.remote_base import declare_protocol, register_binary_sensor, register_trigger, \
     register_dumper, register_action
 
 GiCableData, GiCableBinarySensor, GiCableTrigger, GiCableAction, GiCableDumper = declare_protocol('GiCable')
-GiCable_SCHEMA = cv.Schema({
-    cv.Required(CONF_COMMAND): cv.hex_uint16_t,
-})
+GiCable_SCHEMA = cv.Schema({cv.Required(CONF_COMMAND): cv.hex_uint16_t})
 
 @register_binary_sensor('gicable', GiCableBinarySensor, GiCable_SCHEMA)
 def gicable_binary_sensor(var, config):
@@ -29,9 +19,11 @@ def gicable_binary_sensor(var, config):
 def gicable_trigger(var, config):
     pass
 
+
 @register_dumper('gicable', GiCableDumper)
 def gicable_dumper(var, config):
     pass
+
 
 @register_action('gicable', GiCableAction, GiCable_SCHEMA)
 def gicable_action(var, config, args):
@@ -39,3 +31,5 @@ def gicable_action(var, config, args):
     cg.add(var.set_address(template_))
     template_ = yield cg.templatable(config[CONF_COMMAND], args, cg.uint16)
     cg.add(var.set_command(template_))
+
+CONFIG_SCHEMA = cv.Schema({})
