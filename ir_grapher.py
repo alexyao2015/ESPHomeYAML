4069bbd6e746357c63d85f33d3738eee4d73a09c
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-10-21 18:02:01 -0500

    Fix x axis

--- ir_grapher.py
+++ ir_grapher.py
@@ -23,7 +23,7 @@ print(f"Creating {subplots} subplots")
 
 fig, axs = plt.subplots(subplots, sharex=True, sharey=True, figsize=(100, 20))
 data_plot = {}
-max_value = 0
+max_x_value = 0
 
 for idx, points in enumerate(data):
     data_x = [0]
@@ -34,14 +34,14 @@ for idx, points in enumerate(data):
         data_y.append(1 if point > 0 else -1)
     data_plot[idx] = dict(data_x=data_x, data_y=data_y)
     max_data_x = max(data_x)
-    if max_value < max_data_x:
-        max_value = max_data_x
+    if max_x_value < max_data_x:
+        max_x_value = max_data_x
 
 for plot_idx, data_points in data_plot.items():
     axs[plot_idx].step(data_points["data_x"], data_points["data_y"])
-    axs[plot_idx].set_xlim(0, max_value + 10)
+    axs[plot_idx].set_xlim(0, max_x_value + 10)
     axs[plot_idx].set_ylim(-1.5, 1.5)
-    axs[plot_idx].set_xticks(np.arange(min(data_points["data_x"]), max(data_points["data_x"]), TICK_INTERVAL))
+    axs[plot_idx].set_xticks(np.arange(0, max_x_value, TICK_INTERVAL))
     print(f"Created {plot_idx + 1} of {subplots} subplots")
 current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 print("Saving plot...")
