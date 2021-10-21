c426e8cd5948ea05b19926ba41821addfb489557
Alex <33379584+alexyao2015@users.noreply.github.com>
2021-10-21 17:58:42 -0500

    Cleanup

--- ir_grapher.py
+++ ir_grapher.py
@@ -2,22 +2,26 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
-raw_data = ""
+TICK_INTERVAL = 250
+PLOT_DPI = 100
 
 with open("data.txt", mode="r") as fd:
     raw_data = fd.read()
 
+print(f"Obtained raw data: {raw_data}")
+
 raw_data = raw_data.split("\n")
-raw_data = [raw.split("Received Raw: ")[1].split(", ") for raw in raw_data]
+raw_data = [raw.split("Received Raw: ")[1].rstrip("\x1b").split(", ") for raw in raw_data if raw != ""]
 for raw in raw_data:
     for idx, raw_str in enumerate(raw):
         raw[idx] = int(raw_str)
 
 data = raw_data
+print(f"Cleaned data: {data}")
+subplots = len(data)
+print(f"Creating {subplots} subplots")
 
-TICK_INTERVAL = 250
-
-fig, axs = plt.subplots(len(data), sharex=True, sharey=True, figsize=(100, 20))
+fig, axs = plt.subplots(subplots, sharex=True, sharey=True, figsize=(100, 20))
 data_plot = {}
 max_value = 0
 
@@ -37,6 +41,8 @@ for plot_idx, data_points in data_plot.items():
     axs[plot_idx].step(data_points["data_x"], data_points["data_y"])
     axs[plot_idx].set_xlim(0, max_value + 10)
     axs[plot_idx].set_ylim(-1.5, 1.5)
-    axs[plot_idx].set_xticks(np.arange(min(data_points["data_x"]),max(data_points["data_x"]),TICK_INTERVAL))
+    axs[plot_idx].set_xticks(np.arange(min(data_points["data_x"]), max(data_points["data_x"]), TICK_INTERVAL))
+    print(f"Created {plot_idx + 1} of {subplots} subplots")
 current_time = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-fig.savefig(f'plot-{current_time}.jpg', dpi=100)
+print("Saving plot...")
+fig.savefig(f'plot-{current_time}.jpg', dpi=PLOT_DPI)
